8edd8a33bf3c2af2877d1e11c3ff68e784ffd634
Laurence Roberts <lsjroberts@gmail.com>
2013-01-19 02:47:42 +0000

    google analytics

--- views/pages/data-is-cool.php
+++ views/pages/data-is-cool.php
@@ -1,5 +1,5 @@
 <h1>Data is Cool</h1>
 
-<p><small>Website - <a href="http://www.dataiscool.com">dataiscool.com</a></small></p>
+<p><small>Website - <a href="http://www.dataiscool.co.uk">dataiscool.co.uk</a></small></p>
 
 <p>Data is cool, yep</p>
