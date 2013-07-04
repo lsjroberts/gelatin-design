02fd197b7d21e20ff02710dc8924363708ae06b5
Laurence Roberts <lsjroberts@gmail.com>
2013-07-04 04:18:07 +0100

    Added analytics exception cookie

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -8,17 +8,19 @@
 
         @stylesheets('website')
 
-        <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-18813637-1']);
-        _gaq.push(['_trackPageview']);
+        @if (Cookie::get('laurence') != 'true')
+            <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-18813637-1']);
+            _gaq.push(['_trackPageview']);
 
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-        </script>
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+            </script>
+        @endif
     </head>
     <body>
         <div id="top"></div>
