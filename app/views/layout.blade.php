d074b12a21c8ada5efbd4e1232f9d7dfe3862441
Laurence Roberts <lsjroberts@gmail.com>
2013-07-04 04:09:09 +0100

    Added analytics

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -7,6 +7,18 @@
         <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" type="text/css">
 
         @stylesheets('website')
+
+        <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-18813637-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+        </script>
     </head>
     <body>
         <div id="top"></div>
