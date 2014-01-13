da2a2a63ad75b9ea317839af6ae236b7d376e79f
Laurence Roberts <lsjroberts@gmail.com>
2014-01-13 12:48:30 +0000

    Added office mac local identifier

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Jelly-PC'),
+	'local' => array('Jelly-PC', 'Laurences-MacBook-Air.local'),
 
 ));
 
