f4a6e0c7413164f57d7af6f8bb085cdcc8efc570
Laurence Roberts <lsjroberts@outlook.com>
2014-05-24 18:25:05 +0100

    Updated content and build

--- build.js
+++ build.js
@@ -1,18 +1,30 @@
-var metalsmith = require('metalsmith'),
-    branch     = require('metalsmith-branch'),
-    ignore     = require('metalsmith-ignore'),
-    drafts     = require('metalsmith-drafts'),
-    templates  = require('metalsmith-templates'),
-    markdown   = require('metalsmith-markdown'),
-    permalinks = require('metalsmith-permalinks'),
-    sass       = require('metalsmith-sass');
+var metalsmith  = require('metalsmith'),
+    branch      = require('metalsmith-branch'),
+    ignore      = require('metalsmith-ignore'),
+    drafts      = require('metalsmith-drafts'),
+    templates   = require('metalsmith-templates'),
+    markdown    = require('metalsmith-markdown'),
+    permalinks  = require('metalsmith-permalinks'),
+    collections = require('metalsmith-collections'),
+    sass        = require('metalsmith-sass')
+
+    moment      = require('moment');
 
 metalsmith(__dirname)
     .source('src')
     .destination('public')
+    .use(collections({
+        posts: {
+            pattern: 'blog/*',
+            sortBy: 'date',
+            reverse: true,
+        }
+    }))
+    .use(blogIndexList)
     .use(markdown())
     .use(branch('blog/*')
         .use(drafts())
+        .use(formatDate)
         .use(permalinks({
             pattern: 'blog/post/:slug'
         }))
@@ -30,3 +42,25 @@ metalsmith(__dirname)
     .build(function(err) {
         if (err) throw err;
     });
+
+function formatDate(files, metalsmith, done) {
+    for (file in files) {
+        files[file].date = moment(files[file].date).format('Do MMMM, YYYY');
+    }
+    done();
+}
+
+function blogIndexList(files, metalsmith, done) {
+    var index = files['index.md']
+        posts = metalsmith.data.posts,
+        perPage = 2;
+
+    index.posts = posts.slice(0,perPage);
+    index.numPages = perPage / posts.length;
+
+    done();
+}
+
+function blogTagList(files, metalsmith, done) {
+
+}
--- src/index.md
+++ src/index.md
@@ -1,6 +1,15 @@
 ---
-template: page.html
+template: index.html
 title: Hi
+image: /assets/images/logo-header.png
 ---
 
 My name is Laurence Roberts, I'm a developer who makes websites and games.
+
+Here you'll find stuff on [python](/blog/tag/python), [javascript](/blog/tag/javascript) and [php](/blog/tag/php).
+
+You can find all my open-sourced code at [github](http://github.com/lsjroberts).
+
+And follow my various ramblings and thoughts while coding on [twitter](http://twitter.com/gelatindesign).
+
+---
